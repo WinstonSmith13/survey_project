826622d8172616a9f14c2bfc0cb11f780a939874
WinstonSmith13 <krouk.medy@gmail.com>
2022-12-29 12:21:21 +0100

    router: working on the router.

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,28 +1,10 @@
-<script>
-
-
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  components: {HelloWorld},
-}
+<script setup>
 
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <router-view></router-view>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
