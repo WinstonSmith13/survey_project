4d7befc9cb88a0872fc3a0076c164ff4f2836472
WinstonSmith13 <krouk.medy@gmail.com>
2022-12-30 13:40:01 +0100

    front: working on the login and register front.

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-      alt="Your Company" />
+    <!-- <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+      alt="Logo Company" /> -->
     <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Inscrivez-vous</h2>
     <p class="mt-2 text-center text-sm text-gray-600">
-      Or
+      ou bien
       {{ ' ' }}
-      <router-link :to="{ name: 'Login' }" class="font-medium text-indigo-600 hover:text-indigo-500"> Connectez-vous
+      <router-link :to="{ name: 'Login' }" class="font-medium text-primary hover:text-yellow-500"> Connectez-vous
       </router-link>
     </p>
   </div>
@@ -16,19 +16,19 @@
       <div>
         <label for="fullname" class="sr-only">Nom Prénom</label>
         <input id="fullname" name="name" type="text" autocomplete="name" required=""
-          class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+          class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-primary focus:outline-none focus:ring-primary sm:text-sm"
           placeholder="Nom Prénom" />
       </div>
       <div>
         <label for="email-address" class="sr-only">Email address</label>
         <input id="email-address" name="email" type="email" autocomplete="email" required=""
-          class="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+          class="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-primary focus:outline-none focus:ring-primary sm:text-sm"
           placeholder="Email address" />
       </div>
       <div>
         <label for="password" class="sr-only">Password</label>
         <input id="password" name="password" type="password" autocomplete="current-password" required=""
-          class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+          class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-primary focus:outline-none focus:ring-primary sm:text-sm"
           placeholder="Password" />
       </div>
     </div>
@@ -36,16 +36,16 @@
     <div class="flex items-center justify-between">
       <div class="flex items-center">
         <input id="remember-me" name="remember-me" type="checkbox"
-          class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
+          class="h-4 w-4 rounded border-gray-300 text-primary focus:ring-primary" />
         <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
       </div>
     </div>
 
     <div>
       <button type="submit"
-        class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+        class="group relative flex w-full justify-center rounded-md border border-transparent bg-primary py-2 px-4 text-sm font-medium text-white hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
         <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-          <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+          <LockClosedIcon class="h-5 w-5 text-white group-hover:text-primary" aria-hidden="true" />
         </span>
         Inscription
       </button>
