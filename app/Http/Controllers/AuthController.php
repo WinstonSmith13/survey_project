f33e0a6ee629cf1c3cb30df0779d12b7d8ecf7ca
WinstonSmith13 <krouk.medy@gmail.com>
2022-12-30 16:44:03 +0100

    feat: creating the register backend.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Validation\Rules\Password;
 
-class AuthController
+
+
+class AuthController extends Controller
 {
+    public function register(Request $request)
+    {
+        //Permet d'ajouter des regles pour la validations de l'inscription.
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|string|unique:users,email',
+            'password' => [
+                'required',
+                'confirmed',
+                //on definie les conditions pour un bon mot de passe
+                Password::min(8)->mixedCase()->numbers()->symbols()]
+        ]);
+
+        /** @var User */
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+
+        //Création du token
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+
+    }
 
 }
+
